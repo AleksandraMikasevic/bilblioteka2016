886448e86a25c7fb4313d2a242a0d4ffd390659d
aleksandramikasevic20140040 <amikasevic@gmail.com>
2016-03-17 14:40:32 +0100

    Napravljena prva implementacija interfejsa u vidu klase Biblioteka.

---
+++ Biblioteka2016/src/Biblioteka/Bilblioteka.java
@@ -0,0 +1,39 @@
+package Biblioteka;
+
+import java.util.LinkedList;
+
+import Biblioteka.interfejs.BilbiotekaInterfejs;
+
+public class Bilblioteka implements BilbiotekaInterfejs {
+	private LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		// TODO Auto-generated method stub
+		knjige.add(knjiga);
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		// TODO Auto-generated method stub
+		knjige.remove(knjiga);
+
+	}
+
+	@Override
+	public LinkedList<Knjiga> vratiSveKnjige() {
+		// TODO Auto-generated method stub
+		return knjige;
+	}
+
+	@Override
+	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, long ISBN, String naslov, String izdavac) {
+		// TODO Auto-generated method stub
+		LinkedList<Knjiga> rezultat = new LinkedList<Knjiga>();
+		
+		for (int i = 0; i < knjige.size(); i++) {
+			if(knjige.get(i).getNaslov().contains(naslov)) rezultat.add(knjige.get(i));
+		}
+		return rezultat;
+	}
+
+}
